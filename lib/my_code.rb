35bcb10df6deb0b72e4d36515f354a5b41ba0108
Dominic Chu <dominic.m.chu@gmail.com>
2019-09-20 03:25:25 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,25 @@
-# Your Code Here
+def map(array)
+  new_map = []
+  i = 0
+  while i < array.length do
+    new_map[i] = yield(array[i])
+    i += 1
+  end
+  new_map
+end
+
+def reduce(source_array, starting_point = 0)
+
+  if source_array.all?(Numeric) == true
+    temp_value = starting_point
+  else
+    temp_value = true
+  end
+
+  i = 0
+  while i < source_array.length do
+    temp_value = yield(temp_value, source_array[i])
+    i += 1
+  end
+  temp_value
+end
