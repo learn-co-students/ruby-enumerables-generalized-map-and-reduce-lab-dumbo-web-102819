7efcaa747a60d1b9bc3fd74dc8ff7385cbb818b1
George Skrelja <gskrelja@gmail.com>
2019-10-11 15:31:20 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,26 @@
 # Your Code Here
+def map(source_array)
+  new_array = []
+  counter = 0
+
+  while counter < source_array.length do
+    new_array << yield(source_array[counter])
+    counter += 1
+  end
+  new_array
+end
+
+def reduce(source_array, starting_point = 0)
+  total = starting_point
+  counter = 0
+
+  while counter < source_array.length do
+    total = yield(source_array[counter], total)
+    counter += 1
+  end
+  if total
+    total
+  else
+    false
+  end
+end
